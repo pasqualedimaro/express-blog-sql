b07afcc32e929dadc871fef9c0f9abe9fe2ffe75
pasqualedimaro <pakydm15@gmail.com>
2025-07-12 19:04:12 +0200

    add milestone 2

--- controller/postsController.js
+++ controller/postsController.js
@@ -1,11 +1,18 @@
-// Importiamo i dati dei posts
-// //provo a testare l'errore 500
-// const posts = undefined
-const posts = require('../data/posts')
+//connetto il mio db
+const connection = require('../data/db.js')
 
 
 // INDEX 
-const index = (req, res) => {}
+const index = (req, res) => {
+    //query per ottenere tutti i posts
+    const sql = 'SELECT * FROM posts';
+    // controllo se funziona l'errore
+    connection.query(sql, (err, results) => {
+        if(err)
+            return res.status(500).json(err)
+        res.json(results);
+    })
+};
 
 // SHOW 
 const show = (req, res) => {}
