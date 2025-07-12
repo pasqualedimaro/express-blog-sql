a5e6ebf3f7c13dd9cd32dec4ceea543467537743
pasqualedimaro <pakydm15@gmail.com>
2025-07-12 18:48:49 +0200

    I delete some code that I don't need

--- controller/postsController.js
+++ controller/postsController.js
@@ -4,126 +4,18 @@
 const posts = require('../data/posts')
 
 
-// BONUS
-// INDEX - Restituisce la lista dei post con filtro per tag
-const index = (req, res) => {
-    // Controllo se c'è un parametro di query "tag"
-    const tagFilter = req.query.tag
-    
-    if (tagFilter) {
-        // Filtro i post che contengono il tag specificato
-        const filteredPosts = posts.filter(post => 
-            post.tags.includes(tagFilter)
-        )
-        res.json(filteredPosts)
-    } else {
-        // Se non c'è filtro, restituisco tutti i post
-        res.json(posts)
-    }
-}
+// INDEX 
+const index = (req, res) => {}
 
-// SHOW - Restituisce un singolo post
-const show = (req, res) => {
-    const id = parseInt(req.params.id)
-    const postDetail = posts.find(element => element.id === id)
-    
-    if (!postDetail) {
-        return res.status(404).json({ error: 'Post non trovato' })
-    }
-    
-    res.json(postDetail)
-}
-
-// STORE - Crea un nuovo post
-const store = (req, res) => {
-    console.log('Dati ricevuti:', req.body)
-    
-    // Genero un nuovo ID
-    const newId = posts[posts.length - 1].id + 1
-    
-    const newPost = {
-        id: newId,
-        title: req.body.title,
-        content: req.body.content,
-        image: req.body.image,
-        tags: req.body.tags
-    }
-    
-    posts.push(newPost)
-    
-    res.status(201).json(newPost)
-}
-
-// UPDATE - Modifica totale di un post
-const update = (req, res) => {
-    // Recupero l'id dall'URL e lo trasformo in numero
-    const id = parseInt(req.params.id)
-    
-    // Cerco il post tramite id
-    const post = posts.find(element => element.id === id)
-    
-    // Piccolo controllo
-    if (!post) {
-        res.status(404)
-        return res.json({
-            error: "Not Found",
-            message: "Post non trovato"
-        })
-    }
-    
-    // Aggiorniamo il post
-    post.title = req.body.title
-    post.content = req.body.content
-    post.image = req.body.image
-    post.tags = req.body.tags
-    
-    // Controlliamo i posts
-    console.log(posts)
-    
-    // Restituiamo il post appena aggiornato
-    res.json(post)
-}
-
-// MODIFY - Modifica parziale di un post
-const modify = (req, res) => {
-    const id = req.params.id
-    res.send('modifica parziale del post ' + id)
-}
+// SHOW 
+const show = (req, res) => {}
 
 // DESTROY - Elimina un post
-const destroy = (req, res) => {
-    // Recuperiamo l'id dall'URL e lo trasformiamo in numero
-    const id = parseInt(req.params.id)
-    
-    // Cerchiamo il post tramite id
-    const post = posts.find(element => element.id === id)
-    
-    // Piccolo controllo
-    if (!post) {
-        res.status(404)
-        return res.json({
-            status: 404,
-            error: "Not Found",
-            message: "Post non trovato"
-        })
-    }
-    
-    // Rimuoviamo il post dal menu
-    posts.splice(posts.indexOf(post), 1)
-    
-    // Stampa la lista aggiornata nel terminale
-    console.log('Lista posts aggiornata:', posts)
-    
-    // Restituiamo lo status corretto
-    res.sendStatus(204)
-}
+const destroy = (req, res) => {}
 
 // Esportiamo tutte le funzioni
 module.exports = {
     index,
     show,
-    store,
-    update,
-    modify,
     destroy
 }
--- routes/posts.js
+++ routes/posts.js
@@ -11,15 +11,6 @@ router.get('/', postsController.index)
 // SHOW - Restituisce un singolo post
 router.get('/:id', postsController.show)
 
-// STORE - Crea un nuovo post
-router.post('/', postsController.store)
-
-// UPDATE - Modifica totale di un post
-router.put('/:id', postsController.update)
-
-// MODIFY - Modifica parziale di un post
-router.patch('/:id', postsController.modify)
-
 // DESTROY - Elimina un post
 router.delete('/:id', postsController.destroy)
 
